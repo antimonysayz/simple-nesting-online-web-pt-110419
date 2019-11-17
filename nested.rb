13b8cb303a3062d957b37cd22be1e0f1e52a016d
Anthony Mai <anthony.mai530@gmail.com>
2019-11-17 02:12:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,7 +90,7 @@ def adding_matz
 end
 
 def changing_alan
-	programmer_hash =
+	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -105,13 +105,12 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      
-  programmer_hash[:alan_kay][:known_for] = "GUI"
-  programmer_hash
-
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
 end
+changing_alan
 
 def adding_to_dennis
 	programmer_hash = 
